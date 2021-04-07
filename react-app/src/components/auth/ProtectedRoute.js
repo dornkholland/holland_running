4fb94062768e7bab15d8df7a76c980d5669a611d
dornkholland <dornkholland@gmail.com>
2021-04-07 11:16:14 -0600

    small fixes with protected route and navbar auth

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -6,7 +6,7 @@ const ProtectedRoute = (props) => {
   const user = useSelector((state) => state.auth.user);
 
   return (
-    <Route {...props}>{user ? props.children : <Redirect to="/login" />}</Route>
+    <Route {...props}>{user ? props.children : <Redirect to="/" />}</Route>
   );
 };
 
