aa3159e459892bcffa4673846480551ee7447886
dornkholland <dornkholland@gmail.com>
2021-04-07 10:42:48 -0600

    styled video container

--- react-app/src/components/vid/VideoContainer/VideoLink.js
+++ react-app/src/components/vid/VideoContainer/VideoLink.js
@@ -3,13 +3,16 @@ import { NavLink } from "react-router-dom";
 
 const VideoLink = ({ video }) => {
   return (
-    <div className="vidLink">
-      <NavLink to={`/videos/${video.id}`}>
+    <NavLink to={`/videos/${video.id}`} className="vidLink">
+      <div className="vidLink__content">
         <h1>{video.name}</h1>
         <h2>{video.description}</h2>
-        <h3>Posted: {video.date.split(" ").slice(0, 4).join(" ")}</h3>
-      </NavLink>
-    </div>
+        <h3 className="vidLink__posted">
+          Posted: {video.date.split(" ").slice(0, 4).join(" ")}
+        </h3>
+      </div>
+      <i className="fa fa-play-circle"></i>
+    </NavLink>
   );
 };
 
