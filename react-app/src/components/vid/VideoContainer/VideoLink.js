1c2e95477e35cee4381d670e1c7d770484839598
dornkholland <dornkholland@gmail.com>
2021-04-14 10:18:47 -0600

    styled videolink component

--- react-app/src/components/vid/VideoContainer/VideoLink.js
+++ react-app/src/components/vid/VideoContainer/VideoLink.js
@@ -39,16 +39,17 @@ const VideoLink = ({ video }) => {
         </div>
       ) : null}
       <NavLink to={`/videos/${video.id}`} className="vidLink">
+        <div className="vidLink__thumbnail">
+          <img src={video.thumbnail_url} />
+          <i className="fa fa-play-circle"></i>
+        </div>
         <div className="vidLink__content">
           <h1>{video.name}</h1>
-          <h2>{video.description}</h2>
           <h3 className="vidLink__posted">
             Posted: {video.created_at.split(" ").slice(0, 4).join(" ")}
           </h3>
           {video.demo ? <h3 className="vidLink__demo">Free Demo</h3> : null}
         </div>
-        <img src={video.thumbnail_url} />
-        <i className="fa fa-play-circle"></i>
       </NavLink>
       <Modal
         isOpen={modalIsOpen}
