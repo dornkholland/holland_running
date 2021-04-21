144308504196bde769436e153493ce28ab77e86b
dornkholland <dornkholland@gmail.com>
2021-04-20 21:55:14 -0600

    started styling of  material ui accordions in dropdown

--- react-app/src/components/Navbar/NavbarNoAuth.js
+++ react-app/src/components/Navbar/NavbarNoAuth.js
@@ -7,18 +7,30 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  createMuiTheme,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const NavbarNoAuth = ({ setHamburger, setModalIsOpen, setModalType }) => {
   const dispatch = useDispatch();
-  const [demoToggle, setDemoToggle] = useState(false);
   const closeHamburger = () => {
     setHamburger(false);
   };
 
-  const toggleDemo = () => {
-    setDemoToggle(!demoToggle);
-  };
+  const useStyles = makeStyles({
+    hideBorder: {
+      "&.MuiAccordion-root:before": {
+        backgroundColor: "transparent",
+      },
+    },
+    summary: {
+      "&.MuiAccordionSummary-root": {
+        padding: 0,
+      },
+    },
+  });
+
+  const classes = useStyles();
 
   const openModal = () => {
     closeHamburger();
@@ -46,60 +58,50 @@ const NavbarNoAuth = ({ setHamburger, setModalIsOpen, setModalType }) => {
   };
 
   return (
-    <>
-      {demoToggle === true ? (
-        <ul className="dropdown">
-          <li>
-            <button onClick={toggleDemo}> &lt;&lt; </button>
-          </li>
-          <li>
-            <button onClick={handleDemo}>Demo User</button>
-          </li>
+    <ul>
+      <li>
+        <NavLink
+          to="/"
+          onClick={closeHamburger}
+          exact={true}
+          activeClassName="active"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <button onClick={handleLogin}>Login</button>
+      </li>
+      <li>
+        <button onClick={handleSignup}>Sign Up</button>
+      </li>
+      <Accordion
+        style={{
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          border: "0px solid black",
+          padding: 0,
+        }}
+        className={`nav__dropdown ${classes.hideBorder}`}
+      >
+        <li>
+          <AccordionSummary className={classes.summary}>
+            <button>Demo Users {">>"}</button>
+          </AccordionSummary>
+        </li>
+        <AccordionDetails className="dropdown--on">
+          <ul className="dropdown">
+            <li>
+              <button onClick={handleDemo}>Demo User</button>
+            </li>
 
-          <li>
-            <button onClick={handleOwner}>Demo Owner</button>
-          </li>
-        </ul>
-      ) : (
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              onClick={closeHamburger}
-              exact={true}
-              activeClassName="active"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <button onClick={handleLogin}>Login</button>
-          </li>
-          <li>
-            <button onClick={handleSignup}>Sign Up</button>
-          </li>
-          <li>
-            <button onClick={toggleDemo}>Demo Users {">>"}</button>
-          </li>
-          <Accordion className="nav__dropdown">
-            <AccordionSummary className="dropdown--off">
-              <button>Demo Users {">>"}</button>
-            </AccordionSummary>
-            <AccordionDetails className="dropdown--on">
-              <ul className="dropdown">
-                <li>
-                  <button onClick={handleDemo}>Demo User</button>
-                </li>
-
-                <li>
-                  <button onClick={handleOwner}>Demo Owner</button>
-                </li>
-              </ul>
-            </AccordionDetails>
-          </Accordion>
-        </ul>
-      )}
-    </>
+            <li>
+              <button onClick={handleOwner}>Demo Owner</button>
+            </li>
+          </ul>
+        </AccordionDetails>
+      </Accordion>
+    </ul>
   );
 };
 
